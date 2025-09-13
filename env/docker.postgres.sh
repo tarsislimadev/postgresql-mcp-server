c3202e5ab039d06e223000b2e2c0bf52598c5dc7
Tarsis Lima <br.tmvdl@gmail.com>
2025-09-13 19:30:21 -0300

    Remove useless files

--- env/docker.postgres.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm -d \
-  --network host \
-  --name postgresql-1 \
-  -e POSTGRES_DB=psql \
-  -e POSTGRES_USER=psql \
-  -e POSTGRES_PASSWORD=psql \
-  postgres:alpine
--- env/docker.serve.sh
+++
@@ -1,1 +0,0 @@
-docker run --network host -e "POSTGRES_URL=postgresql://localhost:5432/psql?user=psql&password=psql"   tmvdl/postgresql-mcp-server 
